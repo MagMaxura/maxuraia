83be561f5bdf2a05eb0f9a227e1630e21626c88f
TUWACUAPONIA <tuwacuaponia@gmail.com>
2025-05-23 13:43:04 -0300

    paddle OK 3 1

--- src/components/PaddleButton.jsx
+++ src/components/PaddleButton.jsx
@@ -9,34 +9,47 @@ const PaddleButton = ({ priceId, ctaLabel = 'Comprar ahora', successUrl = 'https
 
   const handleClick = async () => {
     console.log("PaddleButton - Clicked:", { priceId, user, successUrl, cancelUrl });
-      
-        var paddleParams = {
-          items: [{ priceId: String(priceId), quantity: 1 }],
- //         customer: { email: user.email },
- //         customData: { recruiter_id: user.id },
- //         settings: {
- //           successUrl: successUrl,
- //           cancelUrl: cancelUrl,
-          },
-        };
+      //    if (!user) {
+      //      navigate('/register');
+      //      return;
+      //    }
 
-        console.log("PaddleButton - Paddle.Checkout.open params:", paddleParams);
-        window.Paddle.Checkout.open({
-          setting:{    
-            displayMode: "overlay",
-            theme: "light",
-            locale: "en"}
-        },
-          paddleParams);
-        console.log("PaddleButton - Paddle.Checkout.open called successfully");
+      //    if (!user.email_verified) {
+      //      navigate('/register-confirmation');
+      //      return;
+      //    }
+
+      //    if (typeof window !== 'undefined' && window.Paddle) {
+      //      try {
+      //        const price = await getPricePreview(priceId, 'US'); // Assuming US as default country
+      //        console.log("PaddleButton - Price Preview:", price);
+
+     var paddleParams = {
+       items: [{ priceId: String(priceId), quantity: 1 }],
+         // customer: { email: user.email },
+         // customData: { recruiter_id: user.id },
+         // settings: {
+         //   successUrl: successUrl,
+         //   cancelUrl: cancelUrl,
+       };
   };
+
+    console.log("PaddleButton - Paddle.Checkout.open params:", paddleParams);
+    window.Paddle.Checkout.open(paddleParams);
+    console.log("PaddleButton - Paddle.Checkout.open called successfully");
+    //       } catch (error) {
+            //       console.error("PaddleButton - Error fetching price preview:", error);
+                   //     }
+    //      } else {
+   //              console.error('Paddle no está disponible en window.');
+   //               }
+
 
   return (
     <button onClick={handleClick}>
       {ctaLabel}
     </button>
   );
-  console.log("PaddleButton - Paddle.Checkout.open called successfully");
 };
 
 export default PaddleButton;
