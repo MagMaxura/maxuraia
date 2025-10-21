ddda6cc1e184195369dfe3a1d89a747fb66c2a7c
MagMaxura <maxiuranga5@gmail.com>
2025-10-21 11:39:01 -0300

    V 5.4.5 - analisis rapido

--- vite.config.js
+++ vite.config.js
@@ -3,49 +3,53 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 const configHorizonsViteErrorHandler = `
-const observer = new MutationObserver((mutations) => {
-	for (const mutation of mutations) {
-		for (const addedNode of mutation.addedNodes) {
-			if (
-				addedNode.nodeType === Node.ELEMENT_NODE &&
-				(
-					addedNode.tagName?.toLowerCase() === 'vite-error-overlay' ||
-					addedNode.classList?.contains('backdrop')
-				)
-			) {
-				handleViteOverlay(addedNode);
-			}
-		}
-	}
-});
+window.addEventListener('DOMContentLoaded', () => {
+  if (document.documentElement) {
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        for (const addedNode of mutation.addedNodes) {
+          if (
+            addedNode.nodeType === Node.ELEMENT_NODE &&
+            (
+              addedNode.tagName?.toLowerCase() === 'vite-error-overlay' ||
+              addedNode.classList?.contains('backdrop')
+            )
+          ) {
+            handleViteOverlay(addedNode);
+          }
+        }
+      }
+    });
 
-observer.observe(document.documentElement, {
-	childList: true,
-	subtree: true
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true
+    });
+  }
 });
 
 function handleViteOverlay(node) {
-	if (!node.shadowRoot) {
-		return;
-	}
+  if (!node.shadowRoot) {
+    return;
+  }
 
-	const backdrop = node.shadowRoot.querySelector('.backdrop');
+  const backdrop = node.shadowRoot.querySelector('.backdrop');
 
-	if (backdrop) {
-		const overlayHtml = backdrop.outerHTML;
-		const parser = new DOMParser();
-		const doc = parser.parseFromString(overlayHtml, 'text/html');
-		const messageBodyElement = doc.querySelector('.message-body');
-		const fileElement = doc.querySelector('.file');
-		const messageText = messageBodyElement ? messageBodyElement.textContent.trim() : '';
-		const fileText = fileElement ? fileElement.textContent.trim() : '';
-		const error = messageText + (fileText ? ' File:' + fileText : '');
+  if (backdrop) {
+    const overlayHtml = backdrop.outerHTML;
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(overlayHtml, 'text/html');
+    const messageBodyElement = doc.querySelector('.message-body');
+    const fileElement = doc.querySelector('.file');
+    const messageText = messageBodyElement ? messageBodyElement.textContent.trim() : '';
+    const fileText = fileElement ? fileElement.textContent.trim() : '';
+    const error = messageText + (fileText ? ' File:' + fileText : '');
 
-		window.parent.postMessage({
-			type: 'horizons-vite-error',
-			error,
-		}, '*');
-	}
+    window.parent.postMessage({
+      type: 'horizons-vite-error',
+      error,
+    }, '*');
+  }
 }
 `;
 
